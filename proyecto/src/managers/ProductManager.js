4cab4fc514366c43d12c822b44a2074667639130
fer0o <fer0osennin@gmail.com>
2025-11-02 01:40:21 -0600

    feat(products-route): add get route by id

--- proyecto/src/managers/ProductManager.js
+++ proyecto/src/managers/ProductManager.js
@@ -19,6 +19,39 @@ class ProductManager{
             return [];
         }
     }
+    //metodo. para obtener producto por su ID
+    async getProductsById(pid){
+        try{
+            const data = await this.getProducts()
+
+            //recorremos continentes
+            for(const continent of data){
+                //recorremos paises
+                for(const country of continent.countries){
+                    //recorremoos los equipos
+                    for(const team of country.teams){
+                        //buscamos el producto dentro de ese equipo
+                        const product = team.products.find (p => p.id === pid)
+                        if(product){
+                            //si lo encontramos lo retornamos con la info del equipo, pais y continente
+                            return {
+                                continent: continent.continent,
+                                country: country.name,
+                                team: team.name,
+                                ...product
+                            }
+                        }
+                    }
+                }
+            }
+            //si no lo encontramos retornamos null
+            return null;
+        }
+        catch(error){
+            console.error('Error al obtener el producto por ID:', error);
+            throw error;    
+        }
+    }
 }
 
 module.exports = ProductManager;
--- proyecto/src/routes/products.routes.js
+++ proyecto/src/routes/products.routes.js
@@ -16,5 +16,22 @@ router.get('/', async (req, res)=>{
         res.status(500).json({error: 'Error al obtener los productos'});
     }
 })
+//Ruta get para obtener un producto por su ID
+router.get('/:pid', async (req, res)=>{
+    try{
+        const pid = parseInt (req.params.pid)
+        const product = await productManager.getProductsById (pid);
+        //si no existe el producto
+        if(!product){
+            return res.status(404).json({error: 'Producto no encontrado'});
+        }
+        //si existe el producto
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.error('Error al obtener el producto por ID:', error);
+        res.status(500).json({error: 'Error al obtener el producto por ID '});
+    }
+})
 
 module.exports = router;
