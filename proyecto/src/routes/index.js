55231d6aaba9578bb17226fd753b6b51c673017f
fer0o <fer0osennin@gmail.com>
2026-02-08 03:53:20 -0600

    avance de sesion con login

--- proyecto/src/routes/index.js
+++ proyecto/src/routes/index.js
@@ -3,9 +3,11 @@ const router = express.Router();
 
 const productsRouter = require('./products.routes');
 const cartsRouter = require('./carts.routes');
+const sessionsRouter = require('./sessions.routes');
 
 // Usar las rutas
 router.use('/products', productsRouter);
 router.use('/carts', cartsRouter);
+router.use('/sessions', sessionsRouter);
 
 module.exports = router;
---
+++ proyecto/src/utils/jwt.js
@@ -0,0 +1,14 @@
+const jwt = require("jsonwebtoken");
+
+const generateToken = (user) =>{
+    const payload ={
+        id: user._id,
+        email: user.email,
+        role: user.role,
+        first_name: user.first_name,
+        last_name: user.last_name,
+    }
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: "1h" });
+}
+
+module.exports = { generateToken };
