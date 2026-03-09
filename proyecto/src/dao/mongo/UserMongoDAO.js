6b8db584d3659b89809c0cca9e3baaba21f39a3b
fer0o <fer0osennin@gmail.com>
2026-03-08 22:36:24 -0600

    agregado en user dao lo inicial para mail recovery

--- proyecto/src/dao/mongo/UserMongoDAO.js
+++ proyecto/src/dao/mongo/UserMongoDAO.js
@@ -18,6 +18,42 @@ class UserMongoDAO {
         return UserModel.findById(id).select("-password").lean();
     }
 
+    //reset Token
+    async setResetToken(userId, token, expiresAt){
+        return UserModel.findByIdAndUpdate(
+            userId,
+            {
+                $set:{
+                    resetPasswordToken:token,
+                    resetPasswordExpires:expiresAt
+                },
+            },
+            {new:true}
+        ).lean();
+    }
+    //reset Token
+    async findByResetToken (token){
+        return UserModel.findOne({
+            resetPasswordToken:token,
+            resetPasswordExpires:{$gt:new Date()}
+        }).lean();
+    }
+
+    async updatePasswordAndClearReset(userId, newHashedPassword){
+        return UserModel.findByIdAndUpdate(
+            userId,
+            {
+                $set:{ password: newHashedPassword},
+                $unset:{
+                    resetPasswordToken:"",
+                    resetPasswordExpires:""
+                },
+            },
+            {new:true}
+        ).lean();
+    }
+
+
 }
 
 module.exports = new UserMongoDAO();
