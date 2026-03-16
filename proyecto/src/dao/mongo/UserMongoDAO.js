4ad07d01b11f45f6aa64aa4e79a80d1b225c6f20
fer0o <fer0osennin@gmail.com>
2026-03-16 05:41:56 -0600

    refactor DAO de user, pruebas funcionando en user

--- proyecto/src/dao/mongo/UserMongoDAO.js
+++ proyecto/src/dao/mongo/UserMongoDAO.js
@@ -1,55 +1,148 @@
+const mongoose = require("mongoose");
 const UserModel = require("../../models/User.model");
 
+const { ObjectId } = mongoose.Types;
+
+const resolveFilter = (filterOrId) => {
+  if (!filterOrId) {
+    return null;
+  }
+
+  if (typeof filterOrId === "string" || filterOrId instanceof ObjectId) {
+    return { _id: filterOrId };
+  }
+
+  if (typeof filterOrId === "object") {
+    return filterOrId;
+  }
+
+  return null;
+};
+
 class UserMongoDAO {
-  async findByEmail(email) {
-    return UserModel.findOne({ email }).lean();
-  }
+  async create(userData, options = {}) {
+    const { session } = options;
 
-  async findById(id) {
-    return UserModel.findById(id).lean();
-  }
+    if (session) {
+      const [user] = await UserModel.create([userData], { session });
+      return user.toObject();
+    }
 
-  async create(userData) {
     const user = await UserModel.create(userData);
     return user.toObject();
   }
-  async findByIdWithoutPassword(id) {
-    return UserModel.findById(id).select("-password").lean();
+
+  async getAll(filter = {}, options = {}) {
+    const { skip, limit, sort, select, populate, session } = options;
+
+    let query = UserModel.find(filter);
+
+    if (select) {
+      query = query.select(select);
+    }
+
+    if (sort) {
+      query = query.sort(sort);
+    }
+
+    if (populate) {
+      query = query.populate(populate);
+    }
+
+    if (Number.isInteger(skip) && skip >= 0) {
+      query = query.skip(skip);
+    }
+
+    if (Number.isInteger(limit) && limit > 0) {
+      query = query.limit(limit);
+    }
+
+    if (session) {
+      query = query.session(session);
+    }
+
+    return query.lean();
   }
 
-  //reset Token
-  async setResetToken(userId, token, expiresAt) {
-    return UserModel.findByIdAndUpdate(
-      userId,
-      {
-        $set: {
-          resetPasswordToken: token,
-          resetPasswordExpires: expiresAt,
-        },
-      },
-      { new: true },
-    ).lean();
-  }
-  //reset Token
-  async findByResetToken(token) {
-    return UserModel.findOne({
-      resetPasswordToken: token,
-      resetPasswordExpires: { $gt: new Date() },
-    }).lean();
+  async getOne(filterOrId, options = {}) {
+    const filter = resolveFilter(filterOrId);
+
+    if (!filter) {
+      return null;
+    }
+
+    const { select, populate, session } = options;
+    let query = UserModel.findOne(filter);
+
+    if (select) {
+      query = query.select(select);
+    }
+
+    if (populate) {
+      query = query.populate(populate);
+    }
+
+    if (session) {
+      query = query.session(session);
+    }
+
+    return query.lean();
   }
 
-  async updatePasswordAndClearReset(userId, newHashedPassword) {
-    return UserModel.findByIdAndUpdate(
-      userId,
-      {
-        $set: { password: newHashedPassword },
-        $unset: {
-          resetPasswordToken: "",
-          resetPasswordExpires: "",
-        },
-      },
-      { new: true },
-    ).lean();
+  async update(filterOrId, updateFields, options = {}) {
+    const filter = resolveFilter(filterOrId);
+
+    if (!filter) {
+      return null;
+    }
+
+    const {
+      session,
+      runValidators = true,
+      upsert = false,
+      sort,
+      select,
+      populate,
+    } = options;
+
+    const queryOptions = { new: true, runValidators, upsert };
+
+    if (sort) {
+      queryOptions.sort = sort;
+    }
+
+    let query = UserModel.findOneAndUpdate(filter, updateFields, queryOptions);
+
+    if (select) {
+      query = query.select(select);
+    }
+
+    if (populate) {
+      query = query.populate(populate);
+    }
+
+    if (session) {
+      query = query.session(session);
+    }
+
+    return query.lean();
+  }
+
+  async delete(filterOrId, options = {}) {
+    const filter = resolveFilter(filterOrId);
+
+    if (!filter) {
+      return null;
+    }
+
+    const { session } = options;
+    let query = UserModel.findOneAndDelete(filter);
+
+    if (session) {
+      query = query.session(session);
+    }
+
+    return query.lean();
   }
 }
 
--- proyecto/src/repositories/UserRepository.js
+++ proyecto/src/repositories/UserRepository.js
@@ -3,32 +3,73 @@ class UserRepository {
     this.dao = dao;
   }
 
-  findByEmail(email) {
-    return this.dao.findByEmail(email);
+  create(userData, options = {}) {
+    return this.dao.create(userData, options);
   }
 
-  findById(id) {
-    return this.dao.findById(id);
+  getAll(filter = {}, options = {}) {
+    return this.dao.getAll(filter, options);
   }
 
-  create(userData) {
-    return this.dao.create(userData);
+  getOne(filterOrId, options = {}) {
+    return this.dao.getOne(filterOrId, options);
   }
 
-  findByIdWithoutPassword(id) {
-    return this.dao.findByIdWithoutPassword(id);
+  update(filterOrId, updateFields, options = {}) {
+    return this.dao.update(filterOrId, updateFields, options);
   }
 
-  setResetToken(userId, token, expiresAt) {
-    return this.dao.setResetToken(userId, token, expiresAt);
+  delete(filterOrId, options = {}) {
+    return this.dao.delete(filterOrId, options);
   }
 
-  findByResetToken(token) {
-    return this.dao.findByResetToken(token);
+  findById(id, options = {}) {
+    return this.dao.getOne(id, options);
   }
 
-  updatePasswordAndClearReset(userId, newHashedPassword) {
-    return this.dao.updatePasswordAndClearReset(userId, newHashedPassword);
+  findByEmail(email, options = {}) {
+    return this.dao.getOne({ email }, options);
+  }
+
+  findByIdWithoutPassword(id, options = {}) {
+    return this.dao.getOne(id, { ...options, select: "-password" });
+  }
+
+  setResetToken(userId, token, expiresAt, options = {}) {
+    return this.dao.update(
+      userId,
+      {
+        $set: {
+          resetPasswordToken: token,
+          resetPasswordExpires: expiresAt,
+        },
+      },
+      options,
+    );
+  }
+
+  findByResetToken(token, options = {}) {
+    return this.dao.getOne(
+      {
+        resetPasswordToken: token,
+        resetPasswordExpires: { $gt: new Date() },
+      },
+      options,
+    );
+  }
+
+  updatePasswordAndClearReset(userId, newHashedPassword, options = {}) {
+    return this.dao.update(
+      userId,
+      {
+        $set: { password: newHashedPassword },
+        $unset: {
+          resetPasswordToken: "",
+          resetPasswordExpires: "",
+        },
+      },
+      options,
+    );
   }
 }
 
