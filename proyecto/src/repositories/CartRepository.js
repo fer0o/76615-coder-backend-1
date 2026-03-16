c621b8af1331d304747f0cb8c49355505989ad07
fer0o <fer0osennin@gmail.com>
2026-03-16 05:29:17 -0600

    refactor DAO de cart, pruebas funcionando en cart

--- proyecto/src/repositories/CartRepository.js
+++ proyecto/src/repositories/CartRepository.js
@@ -3,32 +3,80 @@ class CartRepository {
     this.dao = dao;
   }
 
-  create(cartData = { products: [] }) {
-    return this.dao.create(cartData);
+  create(cartData = { products: [] }, options = {}) {
+    return this.dao.create(cartData, options);
   }
 
-  findById(cid) {
-    return this.dao.findById(cid);
+  getAll(filter = {}, options = {}) {
+    return this.dao.getAll(filter, options);
+  }
+
+  getOne(filterOrId, options = {}) {
+    return this.dao.getOne(filterOrId, options);
+  }
+
+  update(filterOrId, updateFields, options = {}) {
+    return this.dao.update(filterOrId, updateFields, options);
+  }
+
+  delete(filterOrId, options = {}) {
+    return this.dao.delete(filterOrId, options);
+  }
+
+  findById(cid, options = {}) {
+    return this.dao.getOne(cid, options);
   }
 
   findByIdPopulated(cid, options = {}) {
-    return this.dao.findByIdPopulated(cid, options);
+    return this.dao.getOne(cid, {
+      ...options,
+      populate: "products.product",
+    });
   }
 
-  addProduct(cid, pid) {
-    return this.dao.addProduct(cid, pid);
+  async addProduct(cid, pid, options = {}) {
+    const { session } = options;
+
+    const cart = await this.dao.getOne(cid, { session });
+    if (!cart) {
+      return null;
+    }
+
+    const products = Array.isArray(cart.products) ? cart.products : [];
+
+    const alreadyInCart = products.some(
+      (item) => String(item.product) === String(pid),
+    );
+
+    if (alreadyInCart) {
+      return this.dao.update(
+        { _id: cid, "products.product": pid },
+        { $inc: { "products.$.quantity": 1 } },
+        options,
+      );
+    }
+
+    return this.dao.update(
+      cid,
+      { $push: { products: { product: pid, quantity: 1 } } },
+      options,
+    );
   }
 
-  removeProduct(cid, pid) {
-    return this.dao.removeProduct(cid, pid);
+  removeProduct(cid, pid, options = {}) {
+    return this.dao.update(
+      { _id: cid, "products.product": pid },
+      { $pull: { products: { product: pid } } },
+      options,
+    );
   }
 
-  clear(cid) {
-    return this.dao.clear(cid);
+  clear(cid, options = {}) {
+    return this.dao.update(cid, { $set: { products: [] } }, options);
   }
 
   replaceProducts(cid, products, options = {}) {
-    return this.dao.replaceProducts(cid, products, options);
+    return this.dao.update(cid, { $set: { products } }, options);
   }
 }
 
