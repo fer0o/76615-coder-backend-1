cdd76dec4ed81581996c42567e0bbc6fd977ff3d
fer0o <fer0osennin@gmail.com>
2026-03-16 04:58:15 -0600

    refactor de DAO producto, pruebas funcionando en producto

--- proyecto/src/repositories/ProductRepository.js
+++ proyecto/src/repositories/ProductRepository.js
@@ -3,32 +3,83 @@ class ProductRepository {
     this.dao = dao;
   }
 
-  findAllPaginated(params) {
-    return this.dao.findAllPaginated(params);
+  create(productData, options = {}) {
+    return this.dao.create(productData, options);
   }
 
-  findById(pid) {
-    return this.dao.findById(pid);
+  getAll(filter = {}, options = {}) {
+    return this.dao.getAll(filter, options);
   }
 
-  create(productData) {
-    return this.dao.create(productData);
+  getOne(filterOrId, options = {}) {
+    return this.dao.getOne(filterOrId, options);
   }
 
-  updateById(pid, updateFields) {
-    return this.dao.updateById(pid, updateFields);
+  update(filterOrId, updateFields, options = {}) {
+    return this.dao.update(filterOrId, updateFields, options);
   }
 
-  deleteById(pid) {
-    return this.dao.deleteById(pid);
+  delete(filterOrId, options = {}) {
+    return this.dao.delete(filterOrId, options);
+  }
+
+  async findAllPaginated({ limit = 10, page = 1 } = {}) {
+    const parsedLimit = Number(limit);
+    const parsedPage = Number(page);
+
+    const safeLimit =
+      Number.isInteger(parsedLimit) && parsedLimit > 0 ? parsedLimit : 10;
+    const safePage =
+      Number.isInteger(parsedPage) && parsedPage > 0 ? parsedPage : 1;
+    const skip = (safePage - 1) * safeLimit;
+
+    const [payload, ids] = await Promise.all([
+      this.dao.getAll({}, { skip, limit: safeLimit }),
+      this.dao.getAll({}, { select: "_id" }),
+    ]);
+
+    const totalProducts = ids.length;
+    const totalPages = Math.max(1, Math.ceil(totalProducts / safeLimit));
+
+    return {
+      payload,
+      pagination: {
+        totalProducts,
+        totalPages,
+        page: safePage,
+        limit: safeLimit,
+        hasPrevPage: safePage > 1,
+        hasNextPage: safePage < totalPages,
+      },
+    };
+  }
+
+  findById(pid, options = {}) {
+    return this.dao.getOne(pid, options);
+  }
+
+  updateById(pid, updateFields, options = {}) {
+    return this.dao.update(pid, updateFields, options);
+  }
+
+  deleteById(pid, options = {}) {
+    return this.dao.delete(pid, options);
   }
 
   updateStockById(pid, newStock, options = {}) {
-    return this.dao.updateStockById(pid, newStock, options);
+    return this.dao.update(pid, { $set: { stock: newStock } }, options);
   }
 
   decreaseStockIfAvailable(pid, qty, options = {}) {
-    return this.dao.decreaseStockIfAvailable(pid, qty, options);
+    if (!Number.isInteger(qty) || qty <= 0) {
+      return null;
+    }
+
+    return this.dao.update(
+      { _id: pid, stock: { $gte: qty } },
+      { $inc: { stock: -qty } },
+      options,
+    );
   }
 }
 
